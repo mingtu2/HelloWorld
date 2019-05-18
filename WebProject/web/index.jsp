7bf75d73dda447f12feed41db50bddea9749fbdc
mingtu2 <1151320648@qq.com>
2019-05-18 17:41:56 +0800

    Initial commit

---
+++ WebProject/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/12/13
+  Time: 21:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>四望中学</title>
+  </head>
+  <body>
+  <h2 style="color: red">欢迎来到四望中学</h2>
+  </body>
+</html>
---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Date;
+
+/**
+ * @author mingtu
+ * @create 2018/12/12  22:28
+ */
+public class HelloWorld {
+    public int s;
+    public String ;
+    private int ;
+    private String ;
+
+    
+    
+    /**id**/
+    private int ids;
+    
+    public static void main(String[] args) {
+        System.out.println("dd");
+        System.out.println("Aaaa");
+        System.out.println("Aaaa");
+
+        System.out.println();
+        System.err.println("");
+        Date date = new Date();
+
+
+
+
+
+
+        for (int i = 0; i < 10; i++) {
+            
+        }
+
+
+    }
+    public void shows(){
+        
+    }
+
+    public  void show(){
+        System.out.println("show..");
+    }
+
+
+    public  void setIds(){
+
+    }
+}
